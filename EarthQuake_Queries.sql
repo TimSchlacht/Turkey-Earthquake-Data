91d71572deee5100a843e8919f5121895ae3c65b
TimSzlacht <timtxs96@gmail.com>
2025-06-04 13:19:54 -0700

    Update EarthQuake_Queries.sql

--- EarthQuake_Queries.sql
+++ EarthQuake_Queries.sql
@@ -6,3 +6,11 @@ SELECT
 FROM Earthquake_Data
 GROUP BY YEAR(date)
 ORDER BY quake_year;
+
+
+quake_year quake_count	avg_magnitude
+2020	  10987	        1.79
+2021	  23758	        1.81
+2022	  20997	        1.67
+2023	  74509	        1.82
+2024	  23431	        1.66
