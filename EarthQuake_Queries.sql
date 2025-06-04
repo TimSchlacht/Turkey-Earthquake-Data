aa13e0990eaffdab8fa72909368ed969820d382d
TimSzlacht <timtxs96@gmail.com>
2025-06-04 13:27:46 -0700

    Update EarthQuake_Queries.sql

--- EarthQuake_Queries.sql
+++ EarthQuake_Queries.sql
@@ -14,3 +14,26 @@ quake_year quake_count	avg_magnitude
 2022	  20997	        1.67
 2023	  74509	        1.82
 2024	  23431	        1.66
+
+
+
+
+
+SELECT 
+    province,
+    COUNT(*) AS quake_count
+FROM Earthquake_Data
+where province is not null 
+GROUP BY province
+ORDER BY quake_count DESC;
+
+province	  quake_count
+Kahramanmaras	24400
+Malatya	        21546
+Mugla	        13719
+Izmir	       13588
+Adiyaman	    7250
+Adana	        6193
+Elazig	        4160
+Hatay	        4039
+Aydin	        3876
